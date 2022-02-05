e10bbdf24e87e49071e42d9636505e22078e0a06
Mariiaq <masha_print@ukr.net>
2022-02-05 15:30:17 +0200

    js_lesson_1_homework

---
+++ js/lesson_1/homework.js
@@ -0,0 +1,78 @@
+let h = `hello `;
+let o = `owu `;
+let c = `com `;
+let u = `ua `;
+let number1 = 1;
+let number10 = 10;
+let number999 = -999;
+let number123 = 123;
+let number314 = 3.14;
+let number217 = 2.7;
+let number16 = 16;
+alert(h + o + c + u);
+console.log(h);
+console.log(o);
+console.log(c);
+console.log(u);
+console.log(h, o, c, u);
+document.write(h, o, c, u);
+console.log(number1);
+console.log(number10);
+console.log(number16);
+console.log(number999);
+console.log(number123);
+console.log(number217);
+console.log(number314);
+
+h = `Привіт `;
+o = `чудовий  `;
+c = `сьогодні `;
+u = `день) `;
+alert(h + o + c + u);
+document.write(h, o, c, u);
+console.log(h, o, c, u);
+number1 = 15;
+number10 = 24;
+number999 = -188;
+number123 = 212;
+number314 = 2.2;
+number217 = 7.2;
+number16 = 18;
+console.log(number1);
+console.log(number10);
+console.log(number16);
+console.log(number999);
+console.log(number123);
+console.log(number217);
+console.log(number314);
+let person = {
+    firstName: `Branashko` ,
+    middleName: `Mariia` ,
+    lastName: `Vladyslavivna` ,
+}
+console.log(person);
+
+let name = prompt(`Введіть Ваше ім'я`);
+let name2 = prompt('Введіть ваше По - батькові');
+let age = prompt (`Введіть Ваш вік`);
+alert(`Вітаю, ` + name + ` ` + name2 + ` !` + ' Тобі ' + age +` рік!`)
+
+let a = 100;
+let b = '100';
+let g = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof g);
+
+console.log(5 < 6);
+console.log(5 > 6)
+console.log(5 === 6)
+console.log(5 !== 6)
+console.log(10 === 10)
+console.log(10 == 10)
+console.log(10 > 10)
+console.log(10 < 10)
+console.log(10 !== 10)
+console.log(123 === '123')
+console.log(123 !== '123')
+
