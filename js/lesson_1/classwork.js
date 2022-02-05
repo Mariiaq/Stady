9995ff7db6ba771d960713926a6b970d718d7e6e
Mariiaq <masha_print@ukr.net>
2022-02-05 21:10:09 +0200

    js_lesson_1_homework

---
+++ js/lesson_1/classwork.js
@@ -0,0 +1,59 @@
+
+let arr = [15, 45, 84, 124, 815, 46, 184, 998, 483, 4];
+console.log(arr);
+
+
+let product1 = {
+    title: `book`,
+    name: `abetka`,
+    pages: 115,
+    genre: `fantasy`,
+}
+console.log(product1);
+
+let product2 = {
+    title: `book`,
+    name: `gary potter`,
+    pages: 300,
+    genre: `fantasy`,
+    authors: `Joanne Rowling`,
+}
+console.log(product2);
+let books = [
+    {
+        name: `gary potter 1`,
+        pages: 300,
+        genre: `fantasy`,
+        authors: `Joanne Rowling`,
+},
+    {
+        name: `gary potter 2`,
+        pages: 300,
+        genre: `fantasy`,
+        authors: `Joanne Rowling`,
+    },
+    {
+        name: `gary potter 3`,
+        pages: 300,
+        genre: `fantasy`,
+        authors: `Joanne Rowling`,
+    },
+    {
+        name: `gary potter 4`,
+        pages: 300,
+        genre: `fantasy`,
+        authors: `Joanne Rowling`,
+    },
+]
+console.log(books[0]);
+console.log(books[1]);
+console.log(books[2]);
+console.log(books[3]);
+
+
+
+let height = 23;
+let weight = 10;
+let s = (height * weight);
+console.log(s);
+
